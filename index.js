e3f414eb686f9daf9586c22ac9962ee327fcac92
freestyle09 <jozefrzadkosz09@gmail.com>
2021-02-24 17:01:38 +0100

    fixed default import to commonjs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-export { default as Button } from "atoms/Button";
+const PrimaryButton = require("atoms/Button");
+
+module.exports = {
+  PrimaryButton,
+};
+
+// npm publish
+// npm major 2.0.0
+// npm minor 1.1.0
+// npm patch 0.0.1
