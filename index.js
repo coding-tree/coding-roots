70e8faa6bc5455b787e2aacf41eb0da3e22daf2a
freestyle09 <jozefrzadkosz09@gmail.com>
2021-02-24 17:10:48 +0100

    fixed info

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ module.exports = {
 };
 
 // npm publish
-// npm major 2.0.0
-// npm minor 1.1.0
-// npm patch 0.0.1
+// npm version major -> 2.0.0
+// npm version minor -> 1.1.0
+// npm version patch -> 0.0.1
 
 // git tags
 // https://git-scm.com/book/en/v2/Git-Basics-Tagging
