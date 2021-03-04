72b12fb6e7a9b0c1873325fe9adb8b974d14e655
freestyle09 <jozefrzadkosz09@gmail.com>
2021-03-04 17:59:22 +0100

    add typescript

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-"use strict";
-exports.__esModule = true;
-var Button_1 = require("./atoms/Button");
+import PrimaryButton from "./atoms/Button";
+
 module.exports = {
-    PrimaryButton: Button_1["default"]
+  PrimaryButton,
 };
+
 // npm publish
 // npm version major -> 2.0.0
 // npm version minor -> 1.1.0
 // npm version patch -> 0.0.1
+
 // git tags
 // https://git-scm.com/book/en/v2/Git-Basics-Tagging
